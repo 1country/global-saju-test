656e5e0ebbcc9fbccd484a00ce5ead0e8ce11046
1country <1country.ca@gmail.com>
2025-12-19 03:14:08 -0500

    Update 2_📅_Specific_Day_Forecast.py

--- pages/2_📅_Specific_Day_Forecast.py
+++ pages/2_📅_Specific_Day_Forecast.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="Specific Day Forecast", page_icon="📅", layout=
 # 🔑 [마스터 키 & 검로드 설정]
 UNLOCK_CODE = "MASTER2026"
 PRODUCT_PERMALINK = "specific_day"
-GUMROAD_LINK = "https://gumroad.com/l/선생님의_상품주소" 
+GUMROAD_LINK = "5codes.gumroad.com/l/specific_day" 
 
 st.markdown("""
     <style>
