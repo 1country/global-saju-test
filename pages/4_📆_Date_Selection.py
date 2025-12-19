d4e60f274936140d77ecb99a4a6ceacf06ab9cdb
1country <1country.ca@gmail.com>
2025-12-19 02:58:58 -0500

    Update 4_📆_Date_Selection.py

--- pages/4_📆_Date_Selection.py
+++ pages/4_📆_Date_Selection.py
@@ -12,8 +12,8 @@ st.set_page_config(page_title="Date Selection", page_icon="🗓️", layout="wid
 
 # 🔑 [마스터 키 & 검로드 설정]
 UNLOCK_CODE = "MASTER2026"
-PRODUCT_PERMALINK = "Date_Selection" 
-GUMROAD_LINK = "https://gumroad.com/l/Date_Selection" 
+PRODUCT_PERMALINK = "date_selection" 
+GUMROAD_LINK = "https://gumroad.com/l/date_selection" 
 
 st.markdown("""
     <style>
